08374e789e6911d2c73aee004da0e090e4bd5248
sussurro <sussurro@happypacket.net>
2021-01-14 22:49:11 -0500

    updated build script

--- SharpBuilderAll.bat
+++ SharpBuilderAll.bat
@@ -1,10 +1,9 @@
-# SharpAllTheThings
-# Author: ZephrFish
-# Description: Clones down all the git repos from SharpAllTheThings
-## Written for WSL as it will use VS2019 to compile in BuildAllTheThings.bat
-## Converted so it will run with chocaolatey git in dos
-## Setup
+rem Description: Clones down all the git repos from SharpAllTheThings
+rem Written for WSL as it will use VS2019 to compile in BuildAllTheThings.bat
+rem Converted so it will run with chocaolatey git in dos
+rem Setup
 mkdir SharpBuilderAll
+mkdir Binaries
 cd SharpBuilderAll
 mkdir Execution
 mkdir Persistence
@@ -14,23 +13,19 @@ mkdir CredAccess
 mkdir Discovery
 mkdir LateralMovement
 mkdir Exfil
-#
-## Execution
+
 git clone https://github.com/GhostPack/SharpWMI Execution/SharpWMI
 git clone https://github.com/FSecureLABS/SharpGPOAbuse Execution/SharpGPOAbuse
-#
-## Persistence 
+
 git clone https://github.com/fireeye/SharPersist Persistence/SharPersist
 git clone https://github.com/0xthirteen/SharpStay Persistence/SharpStay
-#
-## Privilege Escalation 
+
 git clone https://github.com/GhostPack/SharpUp PrivEsc/SharpUp 
 git clone https://github.com/GhostPack/Seatbelt PrivEsc/Seatbelt
 git clone https://github.com/rasta-mouse/Watson PrivEsc/Watson
 git clone https://github.com/matterpreter/OffensiveCSharp/ OffensiveCSharp
 git clone https://github.com/CCob/SweetPotato PrivEsc/SweetPotato
-#
-## Defense Evasion 
+
 git clone https://github.com/anthemtotheego/SharpCradle DefenseEvasion/SharpCradle
 git clone https://github.com/eladshamir/Internal-Monologue DefenseEvasion/Internal-Monologue
 git clone https://github.com/b4rtik/ATPMiniDump DefenseEvasion/ATPMiniDump
@@ -39,8 +34,7 @@ git clone https://github.com/Soledge/BlockEtw DefenseEvasion/BlockEtw
 git clone https://github.com/PwnDexter/SharpEDRChecker DefenseEvasion/SharpEDRChecker
 git clone https://github.com/CCob/SharpBlock DefenseEvasion/SharpBlock
 git clone https://github.com/matterpreter/DefenderCheck
-#
-## Credential Access 
+
 git clone https://github.com/Pickfordmatt/SharpLocker CredAccess/SharpLocker
 git clone https://github.com/GhostPack/SharpDPAPI CredAccess/SharpDPAPI
 git clone https://github.com/GhostPack/SharpDump CredAccess/SharpDump
@@ -51,8 +45,7 @@ git clone https://github.com/GhostPack/Rubeus CredAccess/Rubeus
 git clone https://github.com/0x09AL/RdpThief CredAccess/RdpThief
 git clone https://github.com/G0ldenGunSec/SharpSecDump CredAccess/SharpSecDump
 git clone https://github.com/r3nhat/SharpWifiGrabber CredAccess/SharpWifiGrabber
-#
-## Discovery 
+
 git clone https://github.com/BloodHoundAD/SharpHound3 Discovery/SharpHound3
 git clone https://github.com/rasta-mouse/SharpWitness Discovery/SharpWitness
 git clone https://github.com/HunnicCyber/SharpDomainSpray Discovery/SharpDomainSpray
@@ -70,17 +63,17 @@ git clone https://github.com/outflanknl/Spray-AD Discovery/Spray-AD
 git clone https://github.com/outflanknl/Recon-AD Discovery/Recon-AD
 git clone https://github.com/l0ss/Grouper2 Discovery/Grouper2
 git clone https://github.com/cube0x0/SharpMapExec Discovery/SharpMapExec
-#
-## Lateral Movement 
+
 git clone https://github.com/rvrsh3ll/SharpCOM LateralMovement/SharpCOM
 git clone https://github.com/rvrsh3ll/SharpExcel4-DCOM LateralMovement/SharpExcel4-DCOM
 git clone https://github.com/anthemtotheego/SharpExec LateralMovement/SharpExec
 git clone https://github.com/0xthirteen/SharpRDP LateralMovement/SharpRDP
 git clone https://github.com/0xthirteen/SharpMove LateralMovement/SharpMove
 git clone https://github.com/Mr-Un1k0d3r/SCShell LateralMovement/SCShell
-#
-## Exfiltration 
+
 git clone https://github.com/P1CKLES/SharpBox Exfil/SharpBox
 git clone https://github.com/outflanknl/Zipper Exfil/Zipper
-# Finished
+
 cmd.exe /c ..\BuildAllTheThings.bat
+powershell -exec bypass -win hidden -command "Get-ChildItem -Recurse -filter *.exe | %{ $_.FullName }  | select-string
+bin | %{ Copy-Item -Path $_ c:\sharp\Binaries }"
